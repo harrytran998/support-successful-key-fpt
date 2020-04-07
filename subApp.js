d1c947fe3f931a83b7ff78c72ca9d118e55a8735
Demons Light <jetaimefrc@gmail.com>
2020-04-08 00:31:37 +0700

    feat: add flip quizlet

---
+++ subApp.js
@@ -0,0 +1,37 @@
+const { writeFileSync, readFileSync } = require('fs')
+
+const quizletDir = __dirname + '/SOURCES_KEY/PRX301-P2.txt'
+const quizletFileName = 'PRX301-P2.txt'
+/**
+ *
+ * @param {String} txtFile
+ */
+const trimLine = (txtFile) => {
+  let arryTrim = txtFile.split('\n\n')
+  let result = []
+  const regex = '!==='
+  for (let item of arryTrim) {
+    const [term, defination] = item.split(regex)
+    result.push(`${defination} ${regex} ${term} \n\n`)
+  }
+  return result
+}
+
+const transformArrayToTxt = (arrData) => arrData.join('\r\n')
+
+const readAndWriteFile = async (fileName) => {
+  const temp = readFileSync(quizletDir, { encoding: 'utf-8' })
+  const dataArr = trimLine(temp)
+  const resultFileName = `Quizlet_${fileName}`
+  writeFileSync(__dirname + '/' + resultFileName, transformArrayToTxt(dataArr), {
+    flag: 'w+',
+    encoding: 'utf-8',
+  })
+}
+
+const bootstrap = () => {
+  readAndWriteFile(quizletFileName)
+  console.log('🌈 Đã viết xong, ném lên Quizlet và học đi dmm 🥳')
+}
+
+bootstrap()
