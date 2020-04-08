8bac838947f994b8f08d126246b4917bedac30f3
Demons Light <jetaimefrc@gmail.com>
2020-04-08 13:52:15 +0700

    chore: update description methods

--- AppMain.js
+++ AppMain.js
@@ -3,28 +3,27 @@ const { createReadStream, createWriteStream, writeFileSync, readdirSync } = requ
 const { createInterface } = require('readline')
 
 /**
- *
+ * @description Cắt dòng và xử lý nó trả về format sau: question | answer
  * @param {String} line
  */
 const trimLine = (line) => {
   const regex = ' | '
+  // Nếu dòng ấy null => Chỉ lấy 1 dòng duy nhất trong cả file
   if (line === '') return
+  // Chỉ khi nào thoả mãn thì mới xử lý, không thì next
   if (line.includes(regex)) {
     let lineTrim = line.split(regex)
+    // Cho lower case xuống để loại bớt trùng lặp
     lineTrim[0] = lineTrim[0].trimRight().toLowerCase()
     lineTrim[1] = lineTrim[1].trimLeft().toLowerCase()
     return lineTrim.join(regex)
   }
 }
 
-const modifyArr = (arr) => {
-  let temp = []
-  for (let i of arr) {
-    temp.push(i)
-  }
-  return temp
-}
-
+/**
+ * @description Đọc file và trả về 1 array Data tương ứng với format
+ * @param {String} filePath
+ */
 const processLineByLine = async (filePath) => {
   let uniqueValue = new Set()
   try {
@@ -39,14 +38,22 @@ const processLineByLine = async (filePath) => {
       }
     })
     await once(rl, 'close')
-    return modifyArr(Array.from(uniqueValue))
+    return Array.from(uniqueValue)
   } catch (err) {
     console.error(err)
   }
 }
 
+/**
+ * @description Convert data phù hợp để in vào file TXT đúng định dạng
+ * @param {Array} arrData
+ */
 const transformArrayToTxt = (arrData) => arrData.join('\r\n')
 
+/**
+ * @description Tạo file kết quả
+ * @param {String} fileName
+ */
 const removeDuplicateAndWrite = async (fileName) => {
   const dataFile = await processLineByLine(fileName)
   const resultFileName = `R-${fileName}`
@@ -56,10 +63,18 @@ const removeDuplicateAndWrite = async (fileName) => {
   })
 }
 
+/**
+ * @description Đọc tất cả các file trong thư mục chỉ định
+ * @param {String} dirPath
+ */
 const getKeyName = (dirPath = __dirname + '/SOURCES_KEY') => {
   return readdirSync(dirPath)
 }
 
+/**
+ * Hàm main
+ * @description Với mỗi file tương ứng => Lọc và viết file trả về key text tương ứng
+ */
 const bootstrap = () => {
   const keysName = getKeyName()
   keysName.forEach((keyName) => {
